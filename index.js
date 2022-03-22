7ee75c3500df7400d9ee79c7a61817ca4a38d5d2
JustD1d1t <starzyk.dawid.0826@gmail.com>
2022-03-22 15:59:53 +0100

    backend + front

---
+++ index.js
@@ -0,0 +1,244 @@
+// BOOKS
+
+// let books = [
+//   {
+//     title: "DZIENNIK ANNE FRANK",
+//     author: "ANNE FRANK",
+//     description:
+//       "Uznany za jedno z największych dzieł literatury światowej, pamiętnik Anne Frank to dokładna, nad wyraz dojrzała relacja z ponad dwuletniego ukrywania się Anne i całej jej rodziny podczas nazistowskiej okupacji Holandii. Trudno nie czytać „Dziennika…” ze smutkiem i swego rodzaju poczuciem straty, gdy wiemy już, że kryjówkę zdradzono, a bohaterka zmarła w obozie kilka miesięcy później… Lektura trudna przez poczucie nadchodzącej zagłady, a jednocześnie przyjemna, bo Anne w błyskotliwy sposób udało się opisać te ostatnie lata swojego życia.",
+//   },
+//   {
+//     title: "MYŚL! I BOGAĆ SIĘ",
+//     author: "NAPOLEON HILL",
+//     description:
+//       "Ten napisany w 1937 roku poradnik wciąż bije na świecie rekordy popularności. Chociaż może „poradnik” to trochę zbyt uproszczone słowo na opisanie tej książki. Jest to raczej zachęcenie do pracy i przyjęcie pewnej filozofii sukcesu, dzięki której, w przyszłości, dzięki ciężkiej pracy, wysiłkowi i odpowiedniemu nastawieniu, każdy jest w stanie stać się dobrze prosperującym człowiekiem. W każdej dziedzinie życia. Coś dla samorozwoju, budowania motywacji i pewności siebie. Napisane w czasach Wielkiej Depresji, dzisiaj w czasie kryzysu sprawdza się jak nigdy dotąd.",
+//   },
+//   {
+//     title: "PRZEMINĘŁO Z WIATREM",
+//     author: "MARGARET MITCHELL",
+//     description:
+//       "Tej powieści pewnie nikomu nie muszę przedstawiać, tym bardziej, że już wcześniej do niej zachęcałam. Dla przypomnienia to pełna namiętności, miłości i zdrady historia pięknej belle z amerykańskiego Południa: Scarlett O’Hary. A w tle wojna secesyjna, krwawy upadek Południa, obalenie niewolnictwa i wielkie zmiany, nie tylko dla całych Stanów Zjednoczonych, ale przed wszystkim dla głównej bohaterki. I jej niezapomniane, kultowe motto: „Bo jutro też jest dzień”.",
+//   },
+//   {
+//     title: "ZMIERZCH",
+//     author: "STEPHENIE MEYER",
+//     description:
+//       "Bardzo chciałabym nie pisać o tej pozycji, a raczej pozycjach kilku, bo to przecież cztery porządne tomiszcza. Ale najwidoczniej nie mam wyboru 😉 Hit wśród młodzieży. Kultowe powieści nastolatków. Wampiry kontra wilkołaki. Odwieczny konflikt. A w tym wszystkim zagubiona, stłamszona i ciamajdowata Bella, która nie może zdecydować się kogo wybrać, czy bladolicego Edwarda, czy śniadego Jacoba. I cierpi. A potem w końcu wybiera. I jeszcze więcej wampirów i wilkołaków.",
+//   },
+//   {
+//     title: "KOD LEONARDA DA VINCI",
+//     author: "DAN BROWN",
+//     description:
+//       "Jedna z najbardziej popularnych powieści ostatnich lat, wciąż na pierwszym miejscu wśród czytelników Wielkiej Brytanii. Pełen wartkiej akcji, tajemnic i morderstw thriller, którego głównym bohaterem jest Robert Langdon. Ten historyk, profesor, specjalista od symboli zostaje wplątany w odwieczny religijny spisek i niczym współczesny rycerz okrągłego stołu wyrusza na poszukiwanie Świętego Graala. A w tle Paryż i oczywiście obrazy Leonardo da Vinci. Typowy przykład literatury pop, dla każdego kto nie wymaga zbyt wiele.",
+//   },
+//   {
+//     title: "ALCHEMIK",
+//     author: "PAOLO COELHO",
+//     description:
+//       "Jedna z książek, których rewelacyjnego odbioru zupełnie nie rozumiem. Przez wielu uznawana za niesamowitą, pełną odwiecznej mądrości, ekstremalnie przeładowaną filozofią powieść.  W gruncie rzeczy jest wyjątkowo tanim chwytem na zbolałe życiem serca naiwnych czytelników, zapełnioną pustymi „rozważaniami” w stylu: „W życiu bo­wiem is­tnieją rzeczy, o które war­to wal­czyć do sa­mego końca”, czy „To oczywiste, że ludzkość zdoła zachować moc.” Po przeanalizowaniu „filozofii” Coelho, wycięciu nic nieznaczących frazesów zostaje może dziesięć stron słabej prozy. Najlepsza dla wciąż „szukających swojej drogi ” zagubionych gimnazjalistów, prawdziwa filozofia light, bez kalorii i żadnych wartości odżywczych.",
+//   },
+//   {
+//     title: "Władca Pierścieni",
+//     author: "J.R.R. Tolkien",
+//     description:
+//       "Pozycja obowiązkowa. Kultowa powieść, która jest podstawą całego współczesnego fantasy, a sam Tolkien został mianowany „ojcem” gatunku. To on sklasyfikował elfy, krasnoludy, orków i gobliny. To dzięki niemu po Śródziemu biegają hobbity. Można się kłócić skąd czerpał inspiracje. Można próbować ubliżyć jego umiejętnościom kreowania światów. Ale bez Tolkiena popkultura, w tym filmy, literatura, gry komputerowe, byłaby o wiele uboższa… Nawet jeśli dzisiaj staje się modne bycie anty-Tolkienem.",
+//   },
+//   {
+//     title: "HARRY POTTER",
+//     author: "J.K. ROWLING",
+//     description:
+//       "Siedem tomów opowiadających dzieje młodego czarodzieja, który walczy ze swoim odwiecznym wrogiem Voldemortem zmieniły życie całego pokolenia. Przez lata czekaliśmy na kolejne tomy, obawiając się o ukochanych bohaterów, razem z nimi przeżywając rozterki i wykłócając się o przynależność do domów Hogwartu. Potem równie długo czekaliśmy na kolejne części ekranizacji. Rowling udało się odświeżyć literaturę dziecięcą i młodzieżową. Wprowadzić element zapomnianej magii we współczesność. Odrealnić trochę rzeczywistość. Baśń, legenda, thriller i dobrze opowiedziana historia dla małych jak i dla dużych czytelników.",
+//   },
+//   {
+//     title: "WYJĄTKI Z DZIEŁ PRZEWODNICZĄCEGO MAO TSE-TUNGA",
+//     author: "MAO TSE-TUNG",
+//     description:
+//       "Co by tu dużo pisać… Obowiązkowa pozycja dla WSZYSTKICH obywateli Chińskiej Republiki Ludowej. NIE do odrzucenia. NIE do podważenia. Prawie czterdzieści lat po śmierci Mao wciąż na szczycie.",
+//   },
+// ];
+
+//FUNCTION JWT
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const logoutForm = document.getElementById("logout-form");
+const loginForm = document.getElementById("login-form");
+
+function checkTokenExpiration(token) {
+  if (token) {
+    const expiry = JSON.parse(atob(token.split(".")[1])).exp;
+    return Math.floor(new Date().getTime() / 1000) >= expiry;
+  }
+  return;
+}
+function parseJwt(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+  return JSON.parse(jsonPayload);
+}
+
+const isTokenExpired = checkTokenExpiration(localStorage.getItem("token"));
+if (!isTokenExpired && typeof isTokenExpired !== "undefined") {
+  loginForm.classList.add("hide-form");
+  logoutForm.classList.remove("hide-form");
+  addMyBooksSection();
+}
+
+let books;
+let borrowButtons = document.querySelectorAll(".borrow-book");
+let returnButtons = document.querySelectorAll(".return-book");
+
+function addMyBooksSection() {
+  const sectionSelector = document.getElementById("library");
+  sectionSelector.innerHTML += `<option value="borrowed">Wypożyczone książki</option>`;
+}
+
+async function getBooks() {
+  const selectedBooks = document.getElementById("library").value;
+  let person;
+  if (typeof localStorage.getItem("token") === "string") {
+    person = parseJwt(localStorage.getItem("token")).id;
+  }
+  const response = await fetch("http://localhost:4000/books", {
+    method: "POST",
+    body: JSON.stringify({
+      books: selectedBooks,
+      person: person,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  books = data;
+  booksInLibrary.innerHTML = "";
+  books.forEach((book) => {
+    const newBook = document.createElement("div");
+    newBook.classList.add("library__book");
+    let template = `<div class="library__book-details"><h4 class="library__book-title">${book.title}</h4><h4 class="library__book-author"> ${book.author}</h4></div><p class="library__book-description clamp-3">${book.description}</p>`;
+
+    if (
+      selectedBooks === "available" &&
+      localStorage.getItem("token") !== null &&
+      !isTokenExpired
+    ) {
+      template += '<button class="borrow-book">Wypożycz książkę</button>';
+    } else if (
+      selectedBooks === "borrowed" &&
+      localStorage.getItem("token") !== "undefined" &&
+      !isTokenExpired
+    ) {
+      template += '<button class="return-book">Oddaj książkę</button>';
+    }
+    newBook.innerHTML = template;
+    addBooksToLibrary(newBook);
+  });
+  borrowButtons = document.querySelectorAll(".borrow-book");
+  borrowButtons.forEach((button) => {
+    button.addEventListener("click", borrowBook);
+  });
+  returnButtons = document.querySelectorAll(".return-book");
+  returnButtons.forEach((button) => {
+    button.addEventListener("click", returnBook);
+  });
+}
+getBooks();
+const form = document.getElementById("section-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getBooks();
+});
+const availableBooksInLibrary = document.querySelector(
+  ".library__available-books"
+);
+const booksInLibrary = document.querySelector(".library__books");
+
+function addBooksToLibrary(book) {
+  booksInLibrary.append(book);
+}
+function addBooksToAvailable(book) {
+  availableBooksInLibrary.append(book);
+}
+
+//USER
+
+async function login() {
+  const response = await fetch("http://localhost:4000/person/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  if (data.token) {
+    localStorage.setItem("token", data.token);
+    loginForm.classList.add("hide-form");
+    logoutForm.classList.remove("hide-form");
+    email.value = "";
+    password.value = "";
+  }
+  addMyBooksSection();
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  loginForm.classList.remove("hide-form");
+  logoutForm.classList.add("hide-form");
+}
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  login();
+});
+
+logoutForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  logout();
+});
+
+const borrowedBooks = document.querySelector(".library__borrowed-books");
+const availableBooks = document.querySelector(".library__available-books");
+
+async function borrowBook(e) {
+  const tokenData = parseJwt(localStorage.getItem("token"));
+  const book = e.target.closest(".library__book");
+  const bookTitle = book.querySelector(".library__book-title").textContent;
+  book.remove();
+  const response = await fetch("http://localhost:4000/books/borrow", {
+    method: "POST",
+    body: JSON.stringify({
+      title: bookTitle,
+      person: tokenData.id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+async function returnBook(e) {
+  const book = e.target.closest(".library__book");
+  const bookTitle = book.querySelector(".library__book-title").textContent;
+  book.remove();
+  const response = await fetch("http://localhost:4000/books/return", {
+    method: "POST",
+    body: JSON.stringify({
+      title: bookTitle,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
